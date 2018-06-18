132bd7773d06e81eedd35c13af336f1447344f9b
João Vítor Retucci <37112993+JVitorRetucci@users.noreply.github.com>
2018-06-18 17:17:00 -0300

    Add files via upload

---
+++ Vertificar_placa.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+#include<locale.h>
+#include<stdlib.h>
+
+int verificar_placa(char x[]){
+    int aux,i,l=3;
+    char aux2[5];
+
+    x[7] = '\0';
+
+    for(i=0;i<5;i++){
+        aux2[i] = x[l];
+        l++;
+    }
+
+    aux = atoi(aux2);
+
+    if((x[0]=='a')&&(aux%2==0)){
+        return 1;
+    }
+    return 0;
+}
